ccecb9b3469debc71e6b0c44b286c3d4405bd91c
Dragos Sarpe <contact@usdgadget.com>
2018-11-16 13:59:24 +0200

    Added the implementation for findFlights method in the repository

--- flightreservation/src/main/java/com/dgs/flightreservation/controllers/FlightController.java
+++ flightreservation/src/main/java/com/dgs/flightreservation/controllers/FlightController.java
@@ -1,13 +1,16 @@
 package com.dgs.flightreservation.controllers;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.dgs.flightreservation.entities.Flight;
 import com.dgs.flightreservation.repos.FlightRepository;
 
 @Controller
@@ -18,8 +21,9 @@ public class FlightController {
 
 	@PostMapping("/findFlights")
 	public String findFlights(@RequestParam String from, @RequestParam String to, 
-			@RequestParam @DateTimeFormat(pattern="MM-dd-yyyy") Date departureDate) {
-		flightRepository.findFlights(from, to, departureDate);
+			@RequestParam @DateTimeFormat(pattern="MM-dd-yyyy") Date departureDate, ModelMap modelMap) {
+		List<Flight> flights = flightRepository.findFlights(from, to, departureDate); 
+		modelMap.addAttribute("flights", flights); 
 		return "displayFlights";
 	}
 }
