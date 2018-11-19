03ef2fbecdd43b588e4d5c1bcc8a67690d9452ec
Dragos Sarpe <contact@usdgadget.com>
2018-11-19 22:44:58 +0200

    Use the service layer class

--- flightreservation/src/main/java/com/dgs/flightreservation/controllers/ReservationController.java
+++ flightreservation/src/main/java/com/dgs/flightreservation/controllers/ReservationController.java
@@ -9,13 +9,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.dgs.flightreservation.dto.ReservationRequest;
 import com.dgs.flightreservation.entities.Flight;
+import com.dgs.flightreservation.entities.Reservation;
 import com.dgs.flightreservation.repos.FlightRepository;
+import com.dgs.flightreservation.services.ReservationService;
 
 @Controller
 public class ReservationController {
 	
 	@Autowired
 	private FlightRepository flightRepository;
+	
+	@Autowired
+	private ReservationService reservationService;
 
 	@RequestMapping("/showCompleteReservation")
 	public String showCompleteReservation(@RequestParam Long flightId, ModelMap modelMap) {
@@ -25,7 +30,9 @@ public class ReservationController {
 	}
 	
 	@PostMapping("/completeReservation")
-	public String completeReservation(ReservationRequest request) {
-		return null;
+	public String completeReservation(ReservationRequest request, ModelMap modelMap) {
+		Reservation reservation = reservationService.bookFlight(request);
+		modelMap.addAttribute("msg", "Reservation created successfully and the id is " + reservation.getId());
+		return "reservationConfirmation";
 	}
 }
