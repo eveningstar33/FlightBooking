dafd50d358baa64e9966bad9277085852de7bff1
Dragos Sarpe <contact@usdgadget.com>
2018-11-20 22:29:04 +0200

    Added the reservation REST service

--- flightreservation/src/main/java/com/dgs/flightreservation/controllers/ReservationRestController.java
+++ flightreservation/src/main/java/com/dgs/flightreservation/controllers/ReservationRestController.java
@@ -1,10 +1,14 @@
 package com.dgs.flightreservation.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dgs.flightreservation.dto.ReservationUpdateRequest;
 import com.dgs.flightreservation.entities.Reservation;
 import com.dgs.flightreservation.repos.ReservationRepository;
 
@@ -19,6 +23,21 @@ public class ReservationRestController {
 	@GetMapping("/reservations/{id}")
 	public Reservation findReservation(@PathVariable Long id) {
 		return reservationRepository.findById(id).get();
+	}
+	
+	@PutMapping("/reservations")
+	public Reservation updateReservation(ReservationUpdateRequest request) {
+		
+		/*
+		  We are creating this 'request' because we don't want to pass in the entire reservation to do this update.
+		  The client need not pass in the entire reservation. Simply he'll use this ReservationUpdateRequest wrapper 
+		  and he'll pass in these 3 fields id, checkIn, numberOfBags and he will do the update on the reservation.
+		*/
+		
+		Reservation reservation = reservationRepository.findById(request.getId()).get();  
+		reservation.setCheckedIn(request.getCheckIn()); 
+		reservation.setNumberOfBags(request.getNumberOfBags()); 
+		return reservationRepository.save(reservation); 
 	}
 
 }
---
+++ flightreservation/src/main/java/com/dgs/flightreservation/dto/ReservationUpdateRequest.java
@@ -0,0 +1,33 @@
+package com.dgs.flightreservation.dto;
+
+public class ReservationUpdateRequest {
+
+	private Long id;
+	private Boolean checkIn;
+	private int numberOfBags;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Boolean getCheckIn() {
+		return checkIn;
+	}
+
+	public void setCheckIn(Boolean checkIn) {
+		this.checkIn = checkIn;
+	}
+
+	public int getNumberOfBags() {
+		return numberOfBags;
+	}
+
+	public void setNumberOfBags(int numberOfBags) {
+		this.numberOfBags = numberOfBags;
+	}
+
+}
