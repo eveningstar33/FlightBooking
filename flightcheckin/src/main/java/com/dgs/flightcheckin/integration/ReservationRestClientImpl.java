401e66c2a7169f5e3da32a581718288ff3574409
Dragos Sarpe <contact@usdgadget.com>
2018-11-21 22:05:25 +0200

    Added the Model and REST Client classes

---
+++ flightcheckin/src/main/java/com/dgs/flightcheckin/integration/ReservationRestClientImpl.java
@@ -0,0 +1,18 @@
+package com.dgs.flightcheckin.integration;
+
+import com.dgs.flightcheckin.integration.dto.Reservation;
+import com.dgs.flightcheckin.integration.dto.ReservationUpdateRequest;
+
+public class ReservationRestClientImpl implements ReservationRestClient {
+
+	@Override
+	public Reservation findReservation(Long id) {
+		return null;
+	}
+
+	@Override
+	public Reservation updateReservation(ReservationUpdateRequest request) {
+		return null;
+	}
+
+}
