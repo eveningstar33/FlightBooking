7388feb0e26a9e29a9eacd0467e46b9652288256
Dragos Sarpe <contact@usdgadget.com>
2018-11-26 14:15:09 +0200

    Externalize flight reservation properties

--- flightreservation/src/main/java/com/dgs/flightreservation/services/ReservationServiceImpl.java
+++ flightreservation/src/main/java/com/dgs/flightreservation/services/ReservationServiceImpl.java
@@ -3,6 +3,7 @@ package com.dgs.flightreservation.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.dgs.flightreservation.dto.ReservationRequest;
@@ -17,6 +18,9 @@ import com.dgs.flightreservation.util.PDFGenerator;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
+
+	@Value("${com.dgs.flightreservation.itinerary.dirpath}")
+	private String ITINERARY_DIR;
 
 	@Autowired
 	private FlightRepository flightRepository;
@@ -80,8 +84,7 @@ public class ReservationServiceImpl implements ReservationService {
 		LOGGER.info("Saving the reservation: " + reservation);
 		Reservation savedReservation = reservationRepository.save(reservation);
 		
-		String filePath = "/D:/Spring Boot/Spring-Micro-Services/03.microservices/reservations/reservation" 
-							+ savedReservation.getId() + ".pdf";
+		String filePath = ITINERARY_DIR + savedReservation.getId() + ".pdf";
 		LOGGER.info("Generating the itinerary");
 		pdfGenerator.generateItinerary(savedReservation, filePath); 
 		LOGGER.info("Emailing the itinerary");
--- flightreservation/src/main/java/com/dgs/flightreservation/util/EmailUtil.java
+++ flightreservation/src/main/java/com/dgs/flightreservation/util/EmailUtil.java
@@ -8,15 +8,20 @@ import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
-import com.dgs.flightreservation.services.ReservationServiceImpl;
-
 @Component
 public class EmailUtil {
 	
+	@Value("${com.dgs.flightreservation.itinerary.email.body}")
+	private String EMAIL_BODY;
+	
+	@Value("${com.dgs.flightreservation.itinerary.email.subject}")
+	private String EMAIL_SUBJECT;
+
 	@Autowired
 	private JavaMailSender sender;
 	
@@ -29,8 +34,8 @@ public class EmailUtil {
 		try {
 			MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
 			messageHelper.setTo(toAddress); 
-			messageHelper.setSubject("Itinerary for your Flight");
-			messageHelper.setText("Please find your itinerary attached.");
+			messageHelper.setSubject(EMAIL_SUBJECT);
+			messageHelper.setText(EMAIL_BODY);
 			messageHelper.addAttachment("Itinerary", new File(filePath));
 			sender.send(message); 
 		} catch (MessagingException e) {
