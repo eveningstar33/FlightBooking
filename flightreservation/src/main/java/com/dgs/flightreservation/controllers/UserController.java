0846fdfef032472b4db44b4433c5d9a185d364dc
Dragos Sarpe <contact@usdgadget.com>
2018-11-26 10:14:49 +0200

    Added loggers

--- flightreservation/src/main/java/com/dgs/flightreservation/controllers/UserController.java
+++ flightreservation/src/main/java/com/dgs/flightreservation/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.dgs.flightreservation.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -16,6 +18,9 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	// To use logging we need to define a field 
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);  
 
 	@RequestMapping("/showReg")
 	public String showRegistrationPage() {
@@ -35,6 +40,12 @@ public class UserController {
 	
 	@PostMapping("/login")
 	public String login(@RequestParam String email, @RequestParam String password, ModelMap modelMap) {
+		LOGGER.error("ERROR");
+		LOGGER.warn("WARN");
+		LOGGER.info("INFO");
+		LOGGER.debug("DEBUG");
+		LOGGER.trace("TRACE");
+
 		User user = userRepository.findByEmail(email); 
 		if (user.getPassword().equals(password)) {
 			return "findFlights";
