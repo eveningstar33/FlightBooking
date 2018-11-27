f86ad682c3f5452e6ac0b8fcca066924270443e9
Dragos Sarpe <contact@usdgadget.com>
2018-11-27 12:49:19 +0200

    Implemented the getReservation service

--- checkinapp/src/app/services/data.service.ts
+++ checkinapp/src/app/services/data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private _http:Http) { }
+
+  public getReservation(id:number):any {
+      return this._http.get('http://localhost:8080/flightreservation/reservations/' + id)
+      .pipe(map(response => {
+          return response.json();
+      }, 
+      error => {
+          console.error(error);
+      }))
+  }
 }
